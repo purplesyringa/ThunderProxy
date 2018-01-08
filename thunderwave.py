9a5b67b83d35a7039eac151b7aa692ecbaddc79a
Ivanq <imachug@yandex.ru>
2018-01-08 21:08:22 +0300

    Add thunderwave.py

---
+++ thunderwave.py
@@ -0,0 +1,25 @@
+from config import zeronet_directory
+from zerowebsocket import ZeroWebSocket
+import json
+
+class ThunderWave(object):
+	def __init__(self):
+		self.address = "1CWkZv7fQAKxTVjZVrLZ8VHcrN6YGGcdky"
+
+	def listen_for_file_done(self):
+		# Find wrapper_key in sites.json
+		wrapper_key = None
+		with open(zeronet_directory + "data/sites.json", "r") as f:
+			sites = json.loads(f.read())
+			wrapper_key = sites[self.address]["wrapper_key"]
+
+		# Access WebSocket
+		with ZeroWebSocket(wrapper_key) as ws:
+			ws.async("channelJoin", "siteChanged")
+
+			while True:
+				msg = ws.recv()
+				if msg["cmd"] != "setSiteInfo":
+					continue
+
+				print msg["params"]
