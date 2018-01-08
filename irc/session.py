4712dec44453b1e9fb79aee6609953614e02dd8b
Ivanq <imachug@yandex.ru>
2018-01-08 12:37:21 +0300

    Parse messages

--- irc/session.py
+++ irc/session.py
@@ -1,5 +1,5 @@
 from util import debug, critical, ServerError, CommandError
-import time
+import time, re
 
 class Session(object):
 	def __init__(self, conn):
@@ -15,4 +15,42 @@ class Session(object):
 		debug("New session")
 
 		while True:
-			self.recvall()
+			message = self.recvall()
+			if message.strip() == "":
+				continue
+
+			message = self.parseMessage(message)
+
+			command = message["command"][0].upper() + message["command"][1:].lower()
+			command = "command" + command
+
+			if command in dir(self):
+				getattr(command, self)(*args)
+
+	def parseMessage(self, message):
+		prefix = None
+		if message[0] == ":":
+			prefix = message[1:message.index(":")]
+			message = message[message.index(":")+1:]
+
+		message = re.split(r" +", message, maxsplit=1)
+
+		command = message[0]
+
+		params = message[1]
+
+		trailing = None
+		try:
+			index = params.index(":")
+			trailing = params[index+1:]
+			params = params[:index]
+		except ValueError:
+			pass
+
+		params = re.split(r" +", params)
+		if trailing is not None:
+			params.append(trailing)
+
+		return dict(command=command, params=params)
+
+	def command
