5e0f67d3f2adc1538ea8ff1b7d53e567cf551c86
Ivanq <imachug@yandex.ru>
2018-01-09 21:00:21 +0300

    Add CLI client

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from thunderwave import ThunderWave
+import datetime
 
 tw = ThunderWave()
 
-tw.listen_for_file_done()
+messages = tw.get_all_lobby_messages()
+messages.sort(key=lambda x: x["date_added"])
+for message in messages:
+	dt = datetime.datetime.fromtimestamp(message["date_added"] / 1000)
+
+	print "[%s]" % message["key"]
+	print dt.strftime("%Y-%m-%d %H:%M")
+	print ""
+	print message["body"].encode("utf-8")
+	print ""
+	print "----------------------------------------"
+	print ""
