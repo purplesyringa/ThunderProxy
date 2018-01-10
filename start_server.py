e5fa8b482e7e57b2d92241ef1373a6d273ce6edd
Ivanq <imachug@yandex.ru>
2018-01-10 20:04:55 +0300

    Broadcast lobby messages when they are received from ThunderWave

--- start_server.py
+++ start_server.py
@@ -5,3 +5,17 @@ import threading
 import irc.server
 server = irc.server.Server("localhost", 6697, Channel=Channel, User=User)
 threading.Thread(target=server.serve).start()
+
+from thunderwave import ThunderWave
+
+def callback(address):
+	for message in tw.load_new_lobby_messages(address=address):
+		server.broadcast(
+			nick=message["cert_user_id"],
+			username=message["from_address"],
+			to="#lobby",
+			message=message["body"]
+		)
+
+tw = ThunderWave()
+tw.listen_for_file_done(callback)
