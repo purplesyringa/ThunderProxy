6414a5c9725b0aaab045bec6b45cbd83db6a8d03
Ivanq <imachug@yandex.ru>
2018-01-10 19:21:49 +0300

    Load messages from SQLite database

--- thunderwave.py
+++ thunderwave.py
@@ -1,10 +1,13 @@
 from config import zeronet_directory
 from zerowebsocket import ZeroWebSocket
-import json, os
+import json, os, sqlite3
 
 class ThunderWave(object):
 	def __init__(self):
 		self.address = "1CWkZv7fQAKxTVjZVrLZ8VHcrN6YGGcdky"
+
+		self.conn = sqlite3.connect("%sdata/%s/data/ThunderWave2.db" % (zeronet_directory, self.address))
+		self.cursor = self.conn.cursor()
 
 	def listen_for_file_done(self, callback):
 		# Find wrapper_key in sites.json
@@ -48,41 +51,33 @@ class ThunderWave(object):
 
 		return data["cert_user_id"]
 
-	def get_lobby_messages(self, address=None, since=0):
-		if address is None:
-			return self.get_all_lobby_messages(since=since)
+	def get_lobby_messages(self, since=0):
+		messages = self.cursor.execute("""
+			SELECT
+				key, date_added, body,
+				json.cert_user_id,
+				REPLACE(json.directory, "users/", "") AS from_address
+			FROM messages
 
-		cert_user_id = self.get_cert_user_id(address)
-		path = "%sdata/%s/data/users/%s/data.json" % (zeronet_directory, self.address, address)
+			LEFT JOIN json USING (json_id)
 
-		messages = None
-		with open(path, "r") as f:
-			data = json.loads(f.read())
-			messages = data["messages"]
+			WHERE date_added > ?
+			ORDER BY date_added ASC
+		""", (since,))
 
-		messages = [
-			message for message in messages
-			if message["date_added"] > since
-		]
+		messages = [message for message in messages]
 
+		new_messages = []
 		for message in messages:
-			if message.get("key", None) is None:
-				message["key"] = None
-			message["from_address"] = address
-			message["cert_user_id"] = cert_user_id
+			message = dict(
+				key=message[0],
+				date_added=message[1],
+				body=message[2],
+				cert_user_id=message[3],
+				from_address=message[4]
+			)
 
-		return messages
-
-	def get_all_lobby_messages(self, since=0):
-		path = "%sdata/%s/data/users" % (zeronet_directory, self.address)
-
-		(_, dirnames, _) = os.walk(path).next()
-
-		messages = []
-		for dirname in dirnames:
-			try:
-				messages += self.get_lobby_messages(dirname, since=since)
-			except IOError:
-				pass
+			new_messages.append(message)
+		messages = new_messages
 
 		return messages
