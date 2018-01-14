3a3a180a717255776787fc91a3dc3274bdee6dfb
Ivanq <imachug@yandex.ru>
2018-01-14 20:23:24 +0300

    Rename chan.broadcast() to chan.receieveMsg()

--- channel.py
+++ channel.py
@@ -55,7 +55,7 @@ class Channel(object):
 	def set_moderated(self, value):
 		raise NotImplementedError()
 
-	def broadcast(self, user, message):
+	def receiveMsg(self, user, message):
 		for to in self.online:
 			to.receivePrivMsg(user, message, chan=self)
 
@@ -74,7 +74,7 @@ class Channel(object):
 					gitcenter/zeroid.bit or glightstar/kaffie.bit.
 					With regards, Ivanq.
 				""".replace("\n", " ").replace("\t", "") % user.nick
-				self.broadcast(self.tp_user, errmsg)
+				self.receiveMsg(self.tp_user, errmsg)
 				return
 
 			self.tw.send_to_lobby(address=address, body=message)
